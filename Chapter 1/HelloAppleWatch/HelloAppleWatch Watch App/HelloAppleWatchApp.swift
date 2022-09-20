0be6c1d4feb17576bd30f923d97422d3bb8b1ce9
mosariot <mosariot@gmail.com>
2022-09-20 06:21:20 +0300

    Initial commit

---
+++ Chapter 1/HelloAppleWatch/HelloAppleWatch Watch App/HelloAppleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppleWatchApp.swift
+//  HelloAppleWatch Watch App
+//
+//  Created by Александр Воробьев on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppleWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
