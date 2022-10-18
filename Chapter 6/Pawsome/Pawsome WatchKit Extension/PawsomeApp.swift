9ca561f6fb1e61e06aec7f47aa7873a787e36bb7
mosariot <mosariot@gmail.com>
2022-10-18 19:56:22 +0300

    Chapter 6 completed

--- Chapter 6/Pawsome/Pawsome WatchKit Extension/PawsomeApp.swift
+++ Chapter 6/Pawsome/Pawsome WatchKit Extension/PawsomeApp.swift
@@ -17,34 +17,10 @@ struct PawsomeApp: App {
       controller: NotificationController.self,
       category: LocalNotifications.categoryIdentifier
     )
+    
     WKNotificationScene(
       controller: RemoteNotificationController.self,
       category: RemoteNotificationController.categoryIdentifier
     )
-  }
-}
-
-import WatchKit
-import UserNotifications
-
-final class ExtensionDelegate: NSObject, WKExtensionDelefate {
-  func didRegisterForRemoteNotifications(withDeviceToken deviceToken: Data) {
-    print(deviceToket.reduce("") { $0 + String(format: "%02x", $1) })
-  }
-  
-  func applicationDidFinishLaunching() {
-    Task {
-      do {
-        let success = try await UNUserNotificationCenter
-          .current
-          .requestAuthorization(options: [.badge, .sound, .alert])
-        guard success else { return }
-        await MainActor.run {
-          WKExtension.shared().registerForRemoteNotifications()
-        }
-      } catch {
-        print(error.localizedDescription)
-      }
-    }
   }
 }
