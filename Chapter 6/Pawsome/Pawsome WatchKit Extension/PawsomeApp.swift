b3a04cdb07273069a7c64020b2d9ecd75dedbf07
mosariot <56510738+mosariot@users.noreply.github.com>
2022-10-18 11:11:39 +0300

    Implementing notifications in Main App

--- Chapter 6/Pawsome/Pawsome WatchKit Extension/PawsomeApp.swift
+++ Chapter 6/Pawsome/Pawsome WatchKit Extension/PawsomeApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct PawsomeApp: App {
+  @WKExtensionDelegateAdaptor(ExtensionDelegate.self)
+  private var extensionDelegate
   private let local = LocalNotifications()
 
   @SceneBuilder var body: some Scene {
@@ -15,5 +17,34 @@ struct PawsomeApp: App {
       controller: NotificationController.self,
       category: LocalNotifications.categoryIdentifier
     )
+    WKNotificationScene(
+      controller: RemoteNotificationController.self,
+      category: RemoteNotificationController.categoryIdentifier
+    )
+  }
+}
+
+import WatchKit
+import UserNotifications
+
+final class ExtensionDelegate: NSObject, WKExtensionDelefate {
+  func didRegisterForRemoteNotifications(withDeviceToken deviceToken: Data) {
+    print(deviceToket.reduce("") { $0 + String(format: "%02x", $1) })
+  }
+  
+  func applicationDidFinishLaunching() {
+    Task {
+      do {
+        let success = try await UNUserNotificationCenter
+          .current
+          .requestAuthorization(options: [.badge, .sound, .alert])
+        guard success else { return }
+        await MainActor.run {
+          WKExtension.shared().registerForRemoteNotifications()
+        }
+      } catch {
+        print(error.localizedDescription)
+      }
+    }
   }
 }
