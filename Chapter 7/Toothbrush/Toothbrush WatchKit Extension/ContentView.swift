0ff9c9c94c0b71c4986ba96608fa8d86c09999f0
mosariot <mosariot@gmail.com>
2022-10-19 06:13:32 +0300

    Chapter 7 completed

--- Chapter 7/Toothbrush/Toothbrush WatchKit Extension/ContentView.swift
+++ Chapter 7/Toothbrush/Toothbrush WatchKit Extension/ContentView.swift
@@ -2,9 +2,35 @@ import SwiftUI
 
 struct ContentView: View {
   @Environment(\.scenePhase) private var scenePhase
+  @ObservedObject private var model = ContentModel()
 
   var body: some View {
-    Text("Hello, World!")
+    VStack {
+      Button("Start brushing") {
+        model.showGettingReady = true
+      }
+      .disabled(model.roundsLeft != 0)
+      .padding()
+      
+      if let endOfBrushing = model.endOfBrushing, let endOfRound = model.endOfRound {
+        Text("Rounds left: \(model.roundsLeft - 1)")
+        Text("Total time left: \(endOfBrushing, style: .timer)")
+        Text("This round time left: \(endOfRound, style: .timer)")
+      }
+    }
+    .overlay(
+      VStack {
+        if model.showGettingReady {
+          GetReadyView {
+            model.startBrushing()
+          }
+            .frame(width: 125, height: 125)
+            .padding()
+        } else {
+          EmptyView()
+        }
+      }
+    )
     .onChange(of: scenePhase) { print($0) }
   }
 }
