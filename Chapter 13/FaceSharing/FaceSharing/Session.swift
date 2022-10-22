0b9dceb42e7b05fa835329971098ed15303af69d
mosariot <mosariot@gmail.com>
2022-10-22 12:36:10 +0300

    Chapter 13 completed

--- Chapter 13/FaceSharing/FaceSharing/Session.swift
+++ Chapter 13/FaceSharing/FaceSharing/Session.swift
@@ -3,31 +3,44 @@ import WatchConnectivity
 
 final class Session: NSObject, ObservableObject {
   static let shared = Session()
+  
+  @MainActor
+  @Published
+  var showFaceSharing = false
 
   override private init() {
     super.init()
 
-    guard WCSession.isSupported() else {
-      return
-    }
+    guard WCSession.isSupported() else { return }
 
     WCSession.default.delegate = self
     WCSession.default.activate()
+  }
+  
+  private func updateFaceSharing(_ session: WCSession) {
+    let activated = session.activationState == .activated
+    let paired = session.isPaired
+    
+    DispatchQueue.main.async {
+      self.showFaceSharing = activated && paired
+    }
   }
 }
 
 extension Session: WCSessionDelegate {
   func sessionDidBecomeInactive(_ session: WCSession) {
+    updateFaceSharing(session)
   }
 
   func sessionDidDeactivate(_ session: WCSession) {
     WCSession.default.activate()
   }
 
-  func session(
-    _ session: WCSession,
-    activationDidCompleteWith activationState: WCSessionActivationState,
-    error: Error?
-  ) {
+  func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    updateFaceSharing(session)
+  }
+  
+  func sessionWatchStateDidChange(_ session: WCSession) {
+    updateFaceSharing(session)
   }
 }
