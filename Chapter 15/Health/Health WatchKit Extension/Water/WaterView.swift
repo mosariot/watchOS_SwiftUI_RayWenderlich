e6d00283c8da57a4feb25f8f64863a0a0881383d
mosariot <56510738+mosariot@users.noreply.github.com>
2022-10-26 15:24:56 +0300

    WaterView for Chapter 15 completed

--- Chapter 15/Health/Health WatchKit Extension/Water/WaterView.swift
+++ Chapter 15/Health/Health WatchKit Extension/Water/WaterView.swift
@@ -2,6 +2,16 @@ import SwiftUI
 import HealthKit
 
 struct WaterView: View {
+  @State private var consumed = ""
+  @State private var percent = ""
+  @State private var graphData: [WaterGraphData]?
+  
+  private let consumedFormat: MeasurementFormatter = {
+    var fmt = MeasurementFormatter()
+    fmt.unitOptions = .naturalScale
+    return rmt
+  }()
+  
   var body: some View {
     ScrollView {
       VStack {
@@ -17,14 +27,44 @@ struct WaterView: View {
           Text("Please enable water tracking in Apple Health.")
         }
       }
+      HStack {
+        Text("Today:")
+          .font(.headline)
+        Text(consumed)
+          .font(.body)
+      }
+      HStack {
+        Text("Goal:")
+          .font(.headline)
+        Text(percent)
+          .font(.body)
+      }
+      BarChart(data: graphData)
+        .padding()
+    }
+    .task {
+      await updateStatus()
+      try? HealthStore.shared.waterConsumptionGraphData() {
+        self.graphData = $0
+      }
     }
   }
   
   private func logWater(quantity: HKQuantity) {
     Task {
       try await HealthStore.shared.logWater(quantity: quantity)
+      await updateStatus()
     }
   }
+  
+  private func updateStatus() async {
+    guard let (measurement, percent) = try? await HealthStore.shared.currentWaterStatus() else {
+      consumed = "0"
+      percent = "Unknown"
+      return
+    }
+    consumed = consumedFormat.string(from: measurement)
+    self.percent = percent?.formatted(.percent.precision(.fractionLenght(0))) ?? "Unknown"
 }
 
 struct WaterView_Previews: PreviewProvider {
